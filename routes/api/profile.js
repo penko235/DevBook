be8ac81865007e1171f909f3f640e525638a3800
Penko Gadzhev <penko032@gmail.com>
2020-08-04 20:41:20 +0300

    Get github code repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator')
@@ -310,10 +312,39 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error')
+    res.status(500).send('Server Error');
   }
 })
 
+// @route GET api/profile/github/:username
+// @route Get user repos from github
+// @route Public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created: asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.log(error)
+      }
+
+      if (response.statusCode !== 200) {
+        return res.status(400).json({ msg: 'No Github profile found' })
+      }
+
+      res.json(JSON.parse(body));
+    });
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
 
 
 module.exports = router;
