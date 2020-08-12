5dcc0e0f04bafdba2c74a8f50ea161d0d3225cc8
Penko Gadzhev <penko032@gmail.com>
2020-08-12 20:51:48 +0300

    Post & Comments

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,6 +10,8 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <ul>
       <li><Link to="/profiles">Developers</Link></li>
 
+      <li><Link to="/posts">Posts</Link></li>
+
       <li><Link to="/dashboard"><i className="fas fa-user"></i>{''}
         <span className='hide-sm'> Dashboard</span></Link></li>
 
